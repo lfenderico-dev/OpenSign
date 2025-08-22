878bdd7685de7c01cad16485eea91cb40c01dec2
Lorenzo Fenderico <lorenzoFenderico.dev@gmail.com>
2025-08-22 10:29:08 +0200

    some adjustments in the authentication layout

--- OpenSignFrontend/src/components/AuthenticationComp.vue
+++ OpenSignFrontend/src/components/AuthenticationComp.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-transparent flex flex-col gap-4">
-    <div class="flex flex-row gap-4">
+  <div class="bg-transparent flex flex-col gap-4 w-[100%] ">
+    <div class="flex flex-row gap-4 justify-center">
       <button class="text-white bg-neutral-800 px-10 py-4 rounded-full font-bold hover:transition hover:-translate-y-4 active:translate-y-0"><i class="fa-brands fa-google flex items-center justify-center"></i> Google</button>
       <button class="text-white bg-neutral-800 px-10 py-4 rounded-full font-bold hover:transition hover:-translate-y-4 active:translate-y-0"><i class="fa-brands fa-apple flex items-center justify-center"></i> Apple</button>
     </div>
@@ -23,13 +23,17 @@
         <input type="password" class="text-emerald-500 bg-neutral-800 rounded-full text-xl p-2 focus:outline-0" name="password" id="password">
       </div>
 
-      <button class="text-xl mx-auto block my-4 px-[28vw] py-4 rounded-full font-bold bg-emerald-500 text-white shadow-2xl shadow-emerald-900 hover:transition hover:-translate-y-4 active:translate-y-0">Create</button>
+      <button class="text-xl mx-auto block my-4 px-[25%] py-4 rounded-full font-bold bg-emerald-500 text-white shadow-2xl shadow-emerald-900 hover:transition hover:-translate-y-4 active:translate-y-0">{{ buttonMessage }}</button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { defineProps } from 'vue';
 
+defineProps<{
+  buttonMessage: string
+}>();
 </script>
 
 <style scoped>
--- OpenSignFrontend/src/view/LogInSection.vue
+++ OpenSignFrontend/src/view/LogInSection.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-[url(@/assets/photo/AuthBg.webp)] bg-cover bg-top min-h-screen flex flex-col justify-center items-center">
+  <div class="bg-[url(@/assets/photo/AuthBg.webp)] bg-cover bg-bottom min-h-screen flex flex-col justify-center items-center">
     <h1 class="text-white mx-auto text-center pt-4 text-5xl font-bold pb-12">Create an account</h1>
-    <div class="flex justify-center items-center"><AuthenticationComp/></div>
+    <div class="flex justify-center items-center"><AuthenticationComp buttonMessage="Create"/></div>
   </div>
 </template>
 
